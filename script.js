a44f85e2fd29d2dc6aae55bee38654e14bb0203c
chandru98677-sudo <chandru98677@gmail.com>
2026-09-11 22:15:11 +0530

    Add functions to open and close info panels

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Chandru's portfolio loaded successfully!");
+function openPanel(panelId) {
+
+    closePanels();
+
+    document.getElementById(panelId).classList.add("active");
+
+}
+
+function closePanels() {
+
+    document.querySelectorAll(".info-panel").forEach(panel => {
+
+        panel.classList.remove("active");
+
+    });
+
+}
